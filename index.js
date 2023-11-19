e2461daf76d576c8ca9bf24d64ed9cb7caf29fc7
Tova Sauer <sauertova1@gmail.com>
2023-11-19 13:38:44 -0500

    added more console logs

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
-console.log('Hello World!');
-console.log('This is super cool');
-console.log('I LIKE THIS!!!');
-console.log(1 + 3);
-let isGreater = 6 > 3;
-console.log (isGreater);
+// console.log('Hello World!');
+// console.log('This is super cool');
+// console.log('I LIKE THIS!!!');
+// console.log(1 + 3);
+// let isGreater = 6 > 3;
+// console.log (isGreater);
 
-var myName = 'Tova';
+// var myName = 'Tova';
 
-function logMyName(){
-    let myName = 'Tova'
+// function logMyName(){
+//     let myName = 'Tova'
 
-    return () =>{ 
-    console.log('****' , myName);
-    return console.log(myName);
-};
-}
+//     return () =>{ 
+//     console.log('****' , myName);
+//     return console.log(myName);
+// };
+// }
 
-for (var i = 0; i < 5; i++){
-    console.log(i);
-}
+// for (var i = 0; i < 5; i++){
+//     console.log(i);
+// }
 
 // const age = 17;
 // console.log(age);
 
-function birthday(currentAge){
-    return currentAge + 1;
-}
-let age = 17;
-console.log('seventeen', age);
-age = 18;
-console.log('reassigned to eighteen', age);
+// function birthday(currentAge){
+//     return currentAge + 1;
+// }
+// let age = 17;
+// console.log('seventeen', age);
+// age = 18;
+// console.log('reassigned to eighteen', age);
 
-let nextYears;
+// let nextYears;
 
- let nextYear;
+//  let nextYear;
 
- nextYear = birthday(age);
+//  nextYear = birthday(age);
 
-console.log(nextYear);
+// console.log(nextYear);
 
-const pi = 3.14,
-e = 2.718;
-console.log('pi', pi);
-console.log('e', e);
+// const pi = 3.14,
+// e = 2.718;
+// console.log('pi', pi);
+// console.log('e', e);
 
-console.log(typeof pi);
-console.log(typeof e);
-console.log(typeof nextYear);
-console.log(typeof birthday);
-console.log(typeof true);
+// console.log(typeof pi);
+// console.log(typeof e);
+// console.log(typeof nextYear);
+// console.log(typeof birthday);
+// console.log(typeof true);
 
+let myFirstName = 'Tova';
+let myLastName = 'Sauer';
+
+console.log(myFirstName + ' ' + myLastName);
