cb948db29ea2573bae50506255d9fbc0765ff187
Tova Sauer <sauertova1@gmail.com>
2023-11-19 13:14:54 -0500

    added var

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('Hello World!');
+console.log('This is super cool');
+console.log('I LIKE THIS!!!');
+console.log(1 + 3);
+let isGreater = 6 > 3;
+console.log (isGreater);
+
+var myName = 'Tova';
+
+function logMyName(){
+    let myName = 'Tova'
+
+    return () =>{ 
+    console.log('****' , myName);
+    return console.log(myName);
+};
+}
+
+for (var i = 0; i < 5; i++){
+    console.log(i);
+}
